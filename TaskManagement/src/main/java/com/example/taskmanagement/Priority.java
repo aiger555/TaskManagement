28ed81752c1ccac3e8e6b1466a28735ae029a42e
Aigerim Nuralieva <75246512+aiger555@users.noreply.github.com>
2023-10-23 14:30:41 +0600

    Delete TaskManagement directory

--- TaskManagement/src/main/java/com/example/taskmanagement/Priority.java
+++
@@ -1,4 +0,0 @@
-package com.example.taskmanagement;
-
-public enum Priority {
-}
--- TaskManagement/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.taskmanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.taskmanagement to javafx.fxml;
-    exports com.example.taskmanagement;
-}
